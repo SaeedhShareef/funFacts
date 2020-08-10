94fb67087e75126fb2c3b2c5b49daf2281a875d2
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-10 00:16:50 -0400

    Updates

---
+++ precache-manifest.ab272deaba730321339f17c1c0da1cd1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b1ad7709a4621b72db96939ad3b805",
+    "url": "/funFacts/index.html"
+  },
+  {
+    "revision": "a59e6e422dff292ec69c",
+    "url": "/funFacts/static/css/main.61d20079.chunk.css"
+  },
+  {
+    "revision": "46d75cb81c12d7012d3b",
+    "url": "/funFacts/static/js/2.28f850d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/funFacts/static/js/2.28f850d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59e6e422dff292ec69c",
+    "url": "/funFacts/static/js/main.9867b817.chunk.js"
+  },
+  {
+    "revision": "ddb8ffd1baed8f74cbb6",
+    "url": "/funFacts/static/js/runtime-main.a4754d89.js"
+  },
+  {
+    "revision": "4bd208e75244a0a0106326676cdcf381",
+    "url": "/funFacts/static/media/img2.4bd208e7.jpg"
+  },
+  {
+    "revision": "84b634ffbf146c9cc8e0ff5c9a7a4db5",
+    "url": "/funFacts/static/media/img3.84b634ff.jpg"
+  },
+  {
+    "revision": "0c76c3ac7b9feb5975d31975e077cf64",
+    "url": "/funFacts/static/media/img4.0c76c3ac.jpg"
+  },
+  {
+    "revision": "d59166289d3b25df9b02c9fc2bde8233",
+    "url": "/funFacts/static/media/img5.d5916628.jpg"
+  },
+  {
+    "revision": "4d1f7947a4eaf10ee4c9a0d4981ff572",
+    "url": "/funFacts/static/media/markus-spiske-hU9gx8YfVK4-unsplash.4d1f7947.jpg"
+  }
+]);
